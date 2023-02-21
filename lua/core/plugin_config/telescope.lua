8cf1327e07444b6787f34750ab1ed6d741fc8ae8
muratsat <muratsat@users.noreply.github.com>
2023-02-21 13:11:14 +0600

    changed telescope mappings

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,5 +1,14 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
-vim.keymap.set('n', '<leader><leader>', builtin.oldfiles, {})
+local keymap = vim.keymap
+
+-- See `:help telescope.builtin`
+keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Search git files' })
+keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })

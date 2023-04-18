1fb2f57a044c8a8143e4f83afbc16813fc7c2fca
Murat <muratsat@users.noreply.github.com>
2023-04-18 23:01:44 +0600

    added russian normal mode

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -105,7 +105,7 @@ local async = event == "BufWritePost"
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.stylua,
+    -- null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.completion.spell,
   },
